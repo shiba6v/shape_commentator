657cdd60fb10da9a366b802315df058f29ae677b
shiba6v <shiba6v@gmail.com>
2018-12-15 02:22:53 +0900

    Fix IPython version code

--- shape_commentator/shape_commentator.py
+++ shape_commentator/shape_commentator.py
@@ -104,7 +104,7 @@ def code_compile(source):
     return code
 
 # TODO 一度検査した行は通らないようにしたい
-def execute(source,globals=None,locals=None):
+def execute(source,globals,locals=None):
     r"""
     >>> SHAPE_COMMENTATOR_RESULT={};execute('import numpy as np\na = np.array([1,2,3,4,5,6])',globals(),locals());SHAPE_COMMENTATOR_RESULT
     {'2': '(6,),'}
@@ -113,20 +113,21 @@ def execute(source,globals=None,locals=None):
     exec(code, globals, locals)
 
 # Jupyter Notebook上で前のセルを簡単にコメントする
-def comment(source, globals, locals):
+def comment(source, globals, locals=None):
     r"""
     >>> In=['import numpy as np\na = np.array([1,2,3,4,5,6])','print("delete_this")'];comment(In[len(In)-2],globals(),locals())
     import numpy as np
     a = np.array([1,2,3,4,5,6])  #_ (6,),
     """
+    print("a")
     # commentを呼んだセルに対してcommentを呼ばれると厄介なので消す
-    exec("In[len(In)-1] = ''", globals, locals)
-    # globals, localsを乗っ取っているので，このライブラリ内で宣言された変数は引き継がれない．
-    locals['SHAPE_COMMENTATOR_RESULT'] = {}
+    exec("In[len(In)-1] = ''", globals)
+    # globalsを乗っ取っているので，このライブラリ内で宣言された変数は引き継がれない．
+    globals['SHAPE_COMMENTATOR_RESULT'] = {}
     try:
-        execute(source, globals, locals)
+        execute(source, globals)
     finally:
-        write_comment(source, locals['SHAPE_COMMENTATOR_RESULT'], print)
+        write_comment(source, globals['SHAPE_COMMENTATOR_RESULT'], print)
 
 # ソース名.commented.pyにコメント付きソースコードを出力．
 def write_comment(source, SHAPE_COMMENTATOR_RESULT, output_func):
