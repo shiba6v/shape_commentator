92f5531d5b21a430824cb5a1bba71b3693e4400c
shiba6v <shiba6v@gmail.com>
2018-12-31 14:32:52 +0900

    Long list abbreviate

--- shape_commentator/main.py
+++ shape_commentator/main.py
@@ -10,10 +10,22 @@ def SHAPE_COMMENTATOR_tuple_unpacker(tpl_input):
     class SHAPE_COMMENTATOR_TUPLE_UNPACKER_ENDMARK_LIST:
         pass
     result = ""
+    length_remaining = 10
+    too_long = False
+    too_long_dot_yet = False
     tpl_stack = [tpl_input]
     while len(tpl_stack) > 0:
         tpl = tpl_stack.pop()
-        if type(tpl) == tuple or type(tpl) == list:
+        if isinstance(tpl,SHAPE_COMMENTATOR_TUPLE_UNPACKER_ENDMARK_TUPLE):
+            result += "),"
+        elif isinstance(tpl,SHAPE_COMMENTATOR_TUPLE_UNPACKER_ENDMARK_LIST):
+            result += "],"
+        elif too_long:
+            if too_long_dot_yet:
+                result += " ... "
+                too_long_dot_yet = False
+            continue
+        elif type(tpl) == tuple or type(tpl) == list:
             if type(tpl) == tuple:
                 result += "("
                 tpl_stack.append(SHAPE_COMMENTATOR_TUPLE_UNPACKER_ENDMARK_TUPLE())
@@ -24,14 +36,14 @@ def SHAPE_COMMENTATOR_tuple_unpacker(tpl_input):
             tmp.reverse()
             for t in tmp:
                 tpl_stack.append(t)
-        elif isinstance(tpl,SHAPE_COMMENTATOR_TUPLE_UNPACKER_ENDMARK_TUPLE):
-            result += "),"
-        elif isinstance(tpl,SHAPE_COMMENTATOR_TUPLE_UNPACKER_ENDMARK_LIST):
-            result += "],"
         elif hasattr(tpl, "shape"):
             result += str(tpl.shape) + ","
         else:
             result += type(tpl).__name__ + ","
+        length_remaining -= 1
+        if not too_long and length_remaining <= 0:
+            too_long = True
+            too_long_dot_yet = True
     result = result[:-1]
     return result
 """
--- tests/correct_scripts/long_list.py.commented.py
+++ tests/correct_scripts/long_list.py.commented.py
@@ -1,2 +1,2 @@
-li = [1,2,3,4,5,6,7,8,9,10,]  #_ [int,int,int,int,int,int,int,int,int,int,]
-li_and_tu = [(1,2),(3,[4,5]),[6,7],[8,(9,10)]]  #_ [(int,int,),(int,[int,int,],),[int,int,],[int,(int,int,),],]
+li = [1,2,3,4,5,6,7,8,9,10,11]  #_ [int,int,int,int,int,int,int,int,int, ... ]
+li_and_tu = [(1,2),(3,[4,5]),[6,7],[8,(9,10),11]]  #_ [(int,int,),(int,[int,int,],), ... ]
