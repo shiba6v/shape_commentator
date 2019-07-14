5ea4313b387a0dd89bbc0f8cd13717d7107b411b
shiba6v <shiba6v@gmail.com>
2019-07-15 03:02:10 +0900

    Update NBExtensions Jupyter Ext

--- shape_commentator/jupyter_ext.py
+++ shape_commentator/jupyter_ext.py
@@ -3,102 +3,25 @@ from .main import make_comment, clear_comment, SHAPE_COMMENTATOR_ENV
 from IPython.display import Javascript, display
 import json
 
-class Ext():
-    def __init__(self, global_dict):
-        self.env = SHAPE_COMMENTATOR_ENV()
-        self.env.globals = global_dict
+class SHAPE_COMMENTATOR_ENV():
+    def __init__(self):
+        self.globals = {}
+        self.filename = ""
 
-        @register_cell_magic
-        def shape_comment(line, cell):
-            output = []
-            output_func = lambda x: output.append(x)
-            display(Javascript(
-                r"""
-                var cell = Jupyter.notebook.get_selected_cell();
-                var text = cell.get_text();
-                var lines = text.split(/\n/);
-                for(var i=0;i<lines.length;i++){
-                  if (lines[i].indexOf('%%shape_comment') != -1){
-                    lines[i] = "";
-                  }
-                }
-                cell.set_text(lines.join("\n"));
-                """
-            ))
-            make_comment(cell, self.env, output_func)
-            output = "\n".join(output)
-            new_code = json.dumps({"code": output})
-            display(Javascript(
-                f"var new_code_json = {new_code}"+"""
-                var new_code = new_code_json["code"];
-                var cell = Jupyter.notebook.get_selected_cell();
-                cell.set_text(new_code);
-                """
-            ))
+@register_cell_magic
+def shape_comment(line, cell):
+    output = []
+    output_func = lambda x: output.append(x)
+    env = SHAPE_COMMENTATOR_ENV()
+    env.globals = globals()
+    make_comment(cell, env, output_func)
+    output = "\n".join(output)
+    return output
 
-        @register_cell_magic
-        def shape_erase(line, cell):
-            output = []
-            output_func = lambda x: output.append(x)
-            clear_comment(cell, output_func)
-            output = "\n".join(output)
-            new_code = json.dumps({"code": output})
-            display(Javascript(
-                f"var new_code_json = {new_code}"+"""
-                var new_code = new_code_json["code"];
-                var cell = Jupyter.notebook.get_selected_cell();
-                cell.set_text(new_code);
-                """
-            ))
-
-        def create_button():
-            display(Javascript(
-            """
-            var Jupyter = require('base/js/namespace');
-            function shape_comment(){
-                var parent = document.getElementsByClassName("end_space")[0];
-                while (parent.firstChild) {
-                    parent.removeChild(parent.firstChild);
-                }
-                var cell = Jupyter.notebook.get_selected_cell();
-                var orig_text = cell.get_text();
-                cell.set_text("%%shape_comment"+"\\n"+orig_text);
-                cell.execute();
-            }
-            if (document.getElementById('shape_commentator_button')!=null){
-                document.getElementById('shape_commentator_button').remove();
-            }
-            if (document.getElementById('shape_commentator_button')==null) {
-                Jupyter.toolbar.add_buttons_group([{
-                    'label': 'Shape',
-                    'icon': 'fa-comment',
-                    'callback': shape_comment,
-                    'id': 'shape_commentator_button'
-                }]);
-            }
-            
-            
-            function shape_erase(){
-                var parent = document.getElementsByClassName("end_space")[0];
-                while (parent.firstChild) {
-                    parent.removeChild(parent.firstChild);
-                }
-                var cell = Jupyter.notebook.get_selected_cell();
-                var orig_text = cell.get_text();
-                cell.set_text("%%shape_erase"+"\\n"+orig_text);
-                cell.execute();
-            }
-            if (document.getElementById('shape_commentator_erase_button')!=null){
-                document.getElementById('shape_commentator_erase_button').remove();
-            }
-            if (document.getElementById('shape_commentator_erase_button')==null) {
-                Jupyter.toolbar.add_buttons_group([{
-                    'label': 'Shape',
-                    'icon': 'fa-eraser',
-                    'callback': shape_erase,
-                    'id': 'shape_commentator_erase_button'
-                }]);
-            }
-            """))
-
-        create_button()
+@register_cell_magic
+def shape_erase(line, cell):
+    output = []
+    output_func = lambda x: output.append(x)
+    clear_comment(cell, output_func)
+    output = "\n".join(output)
+    return output
